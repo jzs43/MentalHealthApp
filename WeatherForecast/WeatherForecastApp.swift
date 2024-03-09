40ad53a9e9d68a0f088b19f592ba4a0132d30229
jzs43 <jzheshu@gmail.com>
2024-03-08 20:31:52 -0500

    initial

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 蒋哲姝 on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
